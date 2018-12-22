e4dca3e5198097d1bc5dea6d8fa57ee48270d507
npurcella <npurcell@aol.com>
2018-12-22 16:26:59 -0700

    Added remote to VSCode

---
+++ header.php
@@ -0,0 +1,23 @@
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.js" integrity="sha256-0vBSIAi/8FxkNOSKyPEfdGQzFDak1dlqFKBYqBp1yC4="
+        crossorigin="anonymous"></script>
+<header>
+    <div class="container">
+        <h1 class="logo">
+            <img src="img/tinyprints.png" alt="">Desert Dawg Sports
+        </h1>
+        <nav id="main-header" class="site-nav">
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Agility</a></li>
+                <li><a href="#">Freestyle</a></li>
+                <li><a href="#">Friends</a></li>
+                <li><a href="#">Gallery</a></li>
+                <li><a href="#">Calendar</a></li>
+            </ul>
+        </nav>
+        <div class="menu-toggle">
+            <div class="hamburger"></div>
+        </div>
+    </div>
+</header>
